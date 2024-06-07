31b3d2bbbbb97d41a02f2b750f02b6c3b6d7743c
Haise-Sato <161752677+Haise-Sato@users.noreply.github.com>
2024-06-07 13:55:25 -0600

    Add files via upload

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1> Waifus Shop </h1>
+            <p> el mejor lugar para comprar las figuras de tus waifus favoritas </p>
+        </div>
+    </div>
+  )
+};
+export default Banner;
